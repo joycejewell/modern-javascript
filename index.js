f4214b5150e5e8bd70230a0fa0004176d1a4893d
joycejewell <joyceroop@gmail.com>
2022-07-24 11:33:20 -0600

    modern javascript first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var moment = require("moment");
+console.log("Hello from Javascript!");
+
+var datetime = moment().format("MMMM Do YYYY, h:mm:ss a");
+console.log(datetime);
+
+var name = "Stella";
+console.log(`Hello ${name}, how are you?`);
